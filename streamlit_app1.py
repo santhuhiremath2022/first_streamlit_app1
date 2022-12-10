9536dec30c16f3784a1030a3ed65e1a954d067ed
Santosh Hiremath <120244932+santhuhiremath2022@users.noreply.github.com>
2022-12-10 16:03:21 +0530

    Update streamlit_app1.py
    Added smoothy

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -1,7 +1,8 @@
 import streamlit
 streamlit.title('My parents new healthy dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text(' Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Blueberry Oatmeal')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
+streamlit.header('🍌 Build Your Own Fruit Smoothie  🍇')
