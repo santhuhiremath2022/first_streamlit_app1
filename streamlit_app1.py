9f2f2ea4bd3637b1fbda10f55295b54edbe7335e
Santosh Hiremath <120244932+santhuhiremath2022@users.noreply.github.com>
2022-12-12 12:08:08 +0530

    Update streamlit_app1.py
    text_input not text_import in line27 -> fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -24,7 +24,7 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display fruityvice api response
 streamlit.header('fruityvice Fruit Advice')
-fruit_choice = streamlit.text_import('What fruit would you like information about?','Kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered', fruit_choice)
 
 import requests
