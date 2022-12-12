5bb13e48913d2efdec18592bb7822a20acb53038
Santosh Hiremath <120244932+santhuhiremath2022@users.noreply.github.com>
2022-12-12 12:01:55 +0530

    Update streamlit_app1.py
    )missed here streamlit.write('The user entered', fruit_choice)

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -25,7 +25,7 @@ streamlit.dataframe(fruits_to_show)
 #New section to display fruityvice api response
 streamlit.header('fruityvice Fruit Advice')
 fruit_choice = streamlit.text_import('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered', fruit_choice
+streamlit.write('The user entered', fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
